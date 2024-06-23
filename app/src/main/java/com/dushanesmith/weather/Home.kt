7b854c1453b1746db5d95bf5d3b285c98ceecdb2
Dushane Smith <dushanesmith@gmail.com>
2024-06-23 13:37:55 -0400

    Moved the setting up weather recycler view code into a function

--- app/src/main/java/com/dushanesmith/weather/Home.kt
+++ app/src/main/java/com/dushanesmith/weather/Home.kt
@@ -11,11 +11,8 @@ import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.common.MapboxOptions
 import com.mapbox.geojson.Point
-import com.mapbox.maps.CameraOptions
-import com.mapbox.maps.MapInitOptions
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
-import com.mapbox.maps.plugin.Plugin
 import com.mapbox.maps.plugin.gestures.OnMapClickListener
 import com.mapbox.maps.plugin.gestures.addOnMapClickListener
 import dagger.hilt.android.AndroidEntryPoint
@@ -29,19 +26,15 @@ class Home : FragmentActivity(), PermissionsListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        //val mapInitOptions = setUpInitCameraOptions()
-        //val mapView = MapView(this, mapInitOptions)
         val mapView = findViewById<MapView>(R.id.mapView)
-        setUpMapBox(mapView)
         askForLocationPermissions()
+        val weatherRecyclerViewAdapter = setupWeatherRecyclerView()
+        setUpMapBox(mapView, weatherRecyclerViewAdapter)
+    }
 
-        val weatherRecyclerViewAdapter = WeatherRecyclerViewAdapter()
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewWeather)
-        val linearLayoutManager = LinearLayoutManager(this)
-        linearLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.adapter = weatherRecyclerViewAdapter
-
+    fun setUpMapBox(mapView: MapView, weatherRecyclerViewAdapter: WeatherRecyclerViewAdapter) {
+        MapboxOptions.accessToken = getString(R.string.mapbox_access_token)
+        mapView.mapboxMap.loadStyle(Style.MAPBOX_STREETS)
         mapView.mapboxMap.addOnMapClickListener(object : OnMapClickListener {
             override fun onMapClick(point: Point): Boolean {
                 weatherRecyclerViewAdapter.data = homeViewModel.getWeatherResults(
@@ -54,27 +47,14 @@ class Home : FragmentActivity(), PermissionsListener {
         })
     }
 
-    fun setUpMapBox(mapView: MapView) {
-        MapboxOptions.accessToken = getString(R.string.mapbox_access_token)
-        mapView.mapboxMap.loadStyle(Style.MAPBOX_STREETS)
-    }
-
-    fun setUpInitCameraOptions(): MapInitOptions {
-        val initialCameraOptions = CameraOptions.Builder()
-            .center(Point.fromLngLat(-74.0066, 40.7135))
-            .pitch(0.0)
-            .zoom(10.0)
-            .bearing(0.0)
-            .build()
-        Plugin.Mapbox(Plugin.MAPBOX_GESTURES_PLUGIN_ID)
-        val gesturePlugin = Plugin.Mapbox(Plugin.MAPBOX_GESTURES_PLUGIN_ID)
-        val mapInitOptions = MapInitOptions(
-            context = this,
-            plugins = listOf<Plugin>(gesturePlugin),
-            cameraOptions = initialCameraOptions,
-            textureView = true
-        )
-        return mapInitOptions
+    fun setupWeatherRecyclerView(): WeatherRecyclerViewAdapter{
+        val weatherRecyclerViewAdapter = WeatherRecyclerViewAdapter()
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewWeather)
+        val linearLayoutManager = LinearLayoutManager(this)
+        linearLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.adapter = weatherRecyclerViewAdapter
+        return weatherRecyclerViewAdapter
     }
 
     fun askForLocationPermissions() {
