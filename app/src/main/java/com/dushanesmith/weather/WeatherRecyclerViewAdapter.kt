4cc34c551ca1da2567231dc5fae035fa817657b0
Dushane Smith <dushanesmith@gmail.com>
2024-06-23 16:41:27 -0400

    Added date to weather report

--- app/src/main/java/com/dushanesmith/weather/WeatherRecyclerViewAdapter.kt
+++ app/src/main/java/com/dushanesmith/weather/WeatherRecyclerViewAdapter.kt
@@ -9,34 +9,47 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.dushane.weather.data.weather.Daily
+import java.util.Calendar
+
 
 class WeatherRecyclerViewAdapter: RecyclerView.Adapter<WeatherRecyclerViewAdapter.ViewHolder>() {
-    var data: ArrayList<Daily> = arrayListOf<Daily>()
+    var data: ArrayList<Daily> = arrayListOf()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.cardview_item_weather, parent, false)
-        return ViewHolder(parent.context, view)
+        return ViewHolder(parent.context, view, this::date)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(data[position])
+        holder.bind(data[position], position)
     }
 
     override fun getItemCount(): Int {
         return data.size
     }
 
-    class ViewHolder(val context:Context, val itemView: View) : RecyclerView.ViewHolder(itemView){
+    fun date(timeToAdd: Int): String {
+        val calendar = Calendar.getInstance()
+        val stringBuilder = StringBuilder()
+        calendar.add(Calendar.DATE,timeToAdd)
+        stringBuilder.append(calendar.get(Calendar.MONTH)+1).append("/")
+        stringBuilder.append(calendar.get(Calendar.DATE))
+        return stringBuilder.toString()
+    }
+
+    class ViewHolder(val context:Context, val itemView: View, val date: (timeToAdd: Int) -> String) : RecyclerView.ViewHolder(itemView){
+        val dateText = itemView.findViewById<TextView>(R.id.date)
         val imageView = itemView.findViewById<ImageView>(R.id.weatherImage)
         val currentTempResult = itemView.findViewById<TextView>(R.id.tempResult)
         val weatherDescription = itemView.findViewById<TextView>(R.id.weatherDescription)
         val windDirectionResult = itemView.findViewById<TextView>(R.id.windDirectionResult)
         val windSpeedResult = itemView.findViewById<TextView>(R.id.windSpeedResult)
 
-        fun bind(daily: Daily){
+        fun bind(daily: Daily, position: Int){
             val imageUrl = "https://openweathermap.org/img/wn/${daily.weather.get(0).icon}@2x.png"
 
             Glide.with(context).load(imageUrl).into(imageView)
+            dateText.text = date(position)
             currentTempResult.text = daily.temp?.day.toString()
             weatherDescription.text = daily.summary.toString()
             windDirectionResult.text = daily.windDeg?.toString() + "°"
